0f7888b5589fbb59dbe4ca10f88d5a62087beafb
Iman Tabrizian <Tabrizian@outlook.com>
2018-05-25 11:42:37 +0430

    Improve user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 const {Model} = require('@bakjs/mongo')
 const Auth = require('@bakjs/auth')
+const { Schema } = require('@bakjs/mongo')
+
 
 class User extends Auth.User {
   static get $schema () {
-    return Object.assign({}, Auth.User.$schema)
+    return Object.assign({}, Auth.User.$schema, {
+      posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }]
+    })
   }
 }
 
