a2e3b8c7d14a9d68b1d826f2c34467b340b3cf84
Iman Tabrizian <tabrizian@outlook.com>
2018-05-31 22:45:35 +0430

    Add wall post and default post creation

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,12 @@
 const { Controller } = require('bak')
-const { Post } = require('../models')
+const { Post, User } = require('../models')
 const Boom = require('boom')
 
 class PostsController extends Controller {
   init () {
     this.get('/posts', this.showAllPosts)
+    this.get('/posts/wall', this.showAllWall)
+    this.post('/posts/wall/{user}', this.createPostWall)
     this.get('/posts/{post}', this.showPost)
     this.post('/posts/{post}', this.updatePost)
     this.post('/posts', this.createPost)
@@ -12,9 +14,34 @@ class PostsController extends Controller {
   }
 
   async showAllPosts (request, h) {
-    let posts
     try {
-      posts = await Post.find()
+      let user = await User.findById(request.user._id).populate('posts')
+      let posts = []
+      for (let post of user.posts) {
+        if (post.user.equals(request.user._id)) {
+          posts.push(post)
+        }
+      }
+
+
+      return { posts }
+    } catch (e) {
+      console.log(e)
+      throw Boom.badRequest()
+    }
+  }
+
+  async showAllWall(request, h) {
+    try {
+      let user = await User.findById(request.user._id).populate('posts')
+      let posts = []
+      for (let post of user.posts) {
+        if (!post.user.equals(request.user._id)) {
+          posts.push(post)
+        }
+      }
+
+
       return { posts }
     } catch (e) {
       console.log(e)
@@ -43,13 +70,33 @@ class PostsController extends Controller {
 
     try {
       post = new Post(request.payload)
+      post.user = request.user._id
       await post.save()
+      let user = await User.findById(request.user._id)
+      user.posts.push(post)
+      await user.save()
       return post
     } catch (e) {
       console.log(e)
       throw Boom.badRequest()
     }
 
+  }
+
+  async createPostWall (request, h) {
+    let post
+    try {
+      post = new Post(request.payload)
+      post.user = request.user._id
+      await post.save()
+      let user = await User.findById(request.params.user)
+      user.posts.push(post)
+      await user.save()
+      return post
+    } catch (e) {
+      console.log(e)
+      throw Boom.badRequest()
+    }
   }
 
   async updatePost (request, h) {
