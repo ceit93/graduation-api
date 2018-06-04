5e5a713f0da1d72a32fb3c4c2fdf485fe2e8d9a7
Aref <haji4ref@gmail.com>
2018-06-05 02:25:39 +0430

    @ Changes profile into avatar in user schema.

--- models/user.js
+++ models/user.js
@@ -5,14 +5,14 @@ const { Schema } = require('@bakjs/mongo')
 
 class User extends Auth.User {
   static get $visible() {
-    return ['_id', 'name', 'email', 'username', 'posts', 'votes', 'std_numbers']
+    return ['_id', 'name', 'email', 'username', 'posts', 'votes', 'std_numbers','avatar']
   }
 
   static get $schema () {
     return Object.assign({}, Auth.User.$schema, {
       posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
       votes: [{ type: Schema.Types.ObjectId, ref: 'Vote' }],
-      profile: String
+      avatar: String,
     })
   }
 }
