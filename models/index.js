db1b49a6f5b74a06cc25c4d4dc2038cbe6b65c78
Amir Haghighati <amirh2008@gmail.com>
2018-06-07 03:31:00 +0430

    add question to index.js at models directory

--- models/index.js
+++ models/index.js
@@ -3,10 +3,12 @@ const User = require('./user')
 const Qualification = require('./qualification')
 const Vote = require('./vote')
 const Notification = require('./notification')
+const Question = require('./question')
 module.exports = {
   User,
   Post,
   Qualification,
   Vote,
-  Notification
+  Notification,
+  Question
 }
