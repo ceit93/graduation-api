75e082aa6368ac264c88d0f3d8c82bb5da155add
Iman Tabrizian <tabrizian@outlook.com>
2018-06-06 18:44:38 +0430

    Fix gender in users

--- controllers/users.js
+++ controllers/users.js
@@ -32,14 +32,14 @@ class UsersController extends Controller {
 
     if (user._id.equals(request.user._id)) {
       for (let post in user.posts) {
-        let author = await User.findById(user.posts[post].user).select('_id name username std_numbers avatar')
+        let author = await User.findById(user.posts[post].user).select('_id name username std_numbers avatar gender')
         user.posts[post].user = author.toObject()
         toBeDisplayedPosts.push(user.posts[post])
       }
     } else {
       for (let post in user.posts) {
         if (user.posts[post].user.equals(request.user._id) || user.posts[post].approved) {
-          let author = await User.findById(user.posts[post].user).select('_id name username std_numbers avatar')
+          let author = await User.findById(user.posts[post].user).select('_id name username std_numbers avatar gender')
           user.posts[post].user = author.toObject()
           toBeDisplayedPosts.push(user.posts[post])
         }
