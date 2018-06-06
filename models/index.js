35fcd2153dd4175f68f536fedb231056e8871ee1
Amir Haghighati <amirh2008@gmail.com>
2018-06-07 03:41:13 +0430

    add Interview to models

--- models/index.js
+++ models/index.js
@@ -4,11 +4,12 @@ const Qualification = require('./qualification')
 const Vote = require('./vote')
 const Notification = require('./notification')
 const Question = require('./question')
+const Question = require('./interview')
 module.exports = {
   User,
   Post,
   Qualification,
   Vote,
   Notification,
-  Question
+  Interview
 }
