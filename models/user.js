d6cb1610ae6e3e5170d1535a0d9f1efe99950040
Amir Haghighati <amirh2008@gmail.com>
2018-07-23 01:47:38 +0430

    update grad photo

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const registerSchema = new Schema({
 
 class User extends Auth.User {
   static get $visible() {
-    return ['_id', 'name', 'email', 'username', 'posts', 'votes', 'std_numbers','avatar', 'gender', 'authorized', 'interviews', 'is_admin', 'modified_name','addFamily']
+    return ['_id', 'name', 'email', 'username', 'posts', 'votes', 'std_numbers','avatar', 'gender', 'authorized', 'interviews', 'is_admin', 'modified_name','addFamily', 'grad_photo']
   }
 
   static get $schema() {
